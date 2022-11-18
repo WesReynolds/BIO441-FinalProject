8ed9657820016d7a7acae13c610b77a2e86ca5a0
Tian Wong <tianwong02@gmail.com>
2022-11-18 14:26:59 -0800

    restructure mhci

--- mhc_i/run.py
+++ mhc_i/run.py
@@ -4,24 +4,33 @@ import pandas as pd
 from Bio import SeqIO
 import logging
 
-def run():
+def parse_arguments():
     parser = argparse.ArgumentParser()
     parser.add_argument("epitope", help="fasta file containing epitopes")
     parser.add_argument("-alleles", default="ref_alleles.csv", metavar="<ref_alleles.csv>", required=False)
     parser.add_argument("-output", default="out.csv", metavar="<out.csv>", required=False)
-    args = vars(parser.parse_args())
+    return vars(parser.parse_args())
 
+def prepare_request(args):
     epitopes = list(SeqIO.parse(args["epitope"], "fasta"))
     seqs = "".join(f"%3E{epi.id}%0A{epi.seq}" for epi in epitopes)
     alleles_f = pd.read_csv(args["alleles"], converters={"length":str})
     a = ",".join(alleles_f["allele"])
     lens = ",".join(alleles_f["length"])
     logging.debug(f"seq: {seqs}\na: {a}\nlens: {lens}\n\n")
-    
+
+    data = f"method=netmhcpan_el&sequence_text={seqs}&allele={a}&length={lens}"
     headers = {
         'Content-Type': 'application/x-www-form-urlencoded',
     }
-    data = f"method=netmhcpan_el&sequence_text={seqs}&allele={a}&length={lens}"
+
+    return headers, data
+
+def run():
+    logging.basicConfig(filename='mch_i.log', encoding='utf-8', level=logging.WARNING, 
+        format='%(levelname)s: %(asctime)s %(message)s:', datefmt='%m/%d/%Y %H:%M:%S')
+    args = parse_arguments()
+    headers, data = prepare_request(args)
 
     response = requests.post('http://tools-cluster-interface.iedb.org/tools_api/mhci/', headers=headers, data=data)
     logging.debug(f"response: {response}")
@@ -34,6 +43,4 @@ def run():
             f_out.write(str_line+"\n")
 
 if __name__ == "__main__":
-    logging.basicConfig(filename='mch_i.log', encoding='utf-8', level=logging.WARNING, 
-        format='%(levelname)s: %(asctime)s %(message)s:', datefmt='%m/%d/%Y %H:%M:%S')
     run()
